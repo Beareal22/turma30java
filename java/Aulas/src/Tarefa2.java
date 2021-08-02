f58c5eb06dedd0bd56db60a10ea94a4ce9539ab2
Beareal22 <beatriz.santos19@yahoo.com.br>
2021-08-02 20:14:29 -0300

    Projeto

--- java/Aulas/src/Tarefa2.java
+++ java/Aulas/src/Tarefa2.java
@@ -1,4 +1,26 @@
+import java.util.Scanner;
 
 public class Tarefa2 {
+	public static void  main (String args[]) 
+	{
+		Scanner leia = new Scanner (System.in);
+		int anos,meses,dia,Diasvivido,Tanos,Tmeses;
+		 System.out.println("Digite Numero de dias :");
+		  Diasvivido = leia.nextInt();
+		  
+		  
+		           Tanos= Diasvivido/365;
+		           Tmeses=Diasvivido% 365/30;
+		           dia= 365% 30% 30;
+		           
+		             System.out.println("Você tem:" + Tanos);
+		             System.out.println("\nMeses:"+ Tmeses);
+		             System.out.println("\n Dias"+ dia);
+		             
+		                  
+	}
+	
+	
+	
 
 }
---
+++ java/Aulas/src/Tarefa3.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Tarefa3 {
+	public  static void  main (String args[]) {
+		
+	Scanner leia = new  Scanner (System.in);
+	int segundo,minutos,horas,tsegundo = 0,tminutos,thoras;
+	
+		System.out.println("segundo:");
+		segundo=leia.nextInt();
+		System.out.println("minutos");
+		minutos=leia.nextInt();
+		System.out.println("horas");
+		horas=leia.nextInt();
+		
+		     tminutos= tsegundo/60;
+		     thoras=tminutos/60;
+		     
+		    		 
+		
+	}
+}
