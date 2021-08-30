52ff2612c873ecbaff57c5a1520b1c5d5f2c7b7f
Beareal22 <beatriz.santos19@yahoo.com.br>
2021-08-30 17:40:53 -0300

    Game

---
+++ Game/src/main/java/com/LojaDeGames/Game/controller/CategoriaController.java
@@ -0,0 +1,65 @@
+package com.LojaDeGames.Game.controller;
+
+import java.util.List;
+
+import org.hibernate.boot.archive.scan.spi.ClassDescriptor.Categorization;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lojagames.LojaGame.model.Categoria;
+import com.lojagames.LojaGame.repository.CategoriaRepository;
+
+import sun.reflect.generics.repository.ClassRepository;
+
+@RestController
+@RequestMapping("/categoria")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+
+class CategoriaController {
+
+	@Autowired
+	private ClassRepository repository;
+
+	@GetMapping
+	public ResponseEntity<List<Categorization>> getAll() {
+		return ResponseEntity.ok(repository.findAll());
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Categoria> getById(@PathVariable long id) {
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
+	}
+
+	@GetMapping("/genero/{genero}")
+	public ResponseEntity<List<Categoria>> getByTipo(@RequestBody String genero) {
+		return ResponseEntity.ok(repository.findAllByGeneroContainingIgnoreCase(genero));
+	}
+
+	@PostMapping
+	public ResponseEntity<Categoria> post(@RequestBody Categorization categoria) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(categoria));
+	}
+
+	@PutMapping
+	public ResponseEntity<Categoria> put(@RequestBody Categoria categoria) {
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(categoria));
+	}
+
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+		repository.deleteById(id);
+	}
+
+}
+
+}
---
+++ Game/src/main/java/com/LojaDeGames/Game/repository/ProdutoRepository.java
@@ -0,0 +1,16 @@
+package com.LojaDeGames.Game.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.LojaDeGames.Game.Model.Produto;
+
+@Repository
+
+public interface ProdutoRepository extends JpaRepository<Produto,Long> {
+	
+	public List<Produto>FindAllByModeloContainingIgnoreCase(String modelo);
+	
+
+}
